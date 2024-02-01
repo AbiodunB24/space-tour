71cb98244ec654ba6d21db3385543c587e78f3b6
Babatunde Balogun <balogunbabatunde42@gmail.com>
2024-02-01 16:38:29 +0100

    destination page

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import bg from "../assets/destination/background-destination-desktop.jpg";
 import data from "../assets/data.json";
+import bgmobile from "../assets/destination/background-destination-mobile.jpg"
 
 const Destination = () => {
   const [destination] = useState(data.destinations);
@@ -17,18 +18,18 @@ const Destination = () => {
 
   return (
     <div
-      style={{ backgroundImage: `url(${bg})` }}
-      className="h-screen bg-cover"
+      style={{ backgroundImage: `url(${bg})`  }}
+      className="h-screen bg-cover overflow-hidden "
     >
-      <h2 className="text-white pt-32 pl-36 flex flex-row text-[28px] barlow-condensed-regular">
+      <h2 className="text-white pt-32 lg:pl-36 lg:justify-start justify-center items-center flex flex-row lg:text-[28px] text-base barlow-condensed-regular">
         {" "}
         <b className="pr-8 text-slate-100/10 backdrop-blur-1x1">01 </b>{" "}
         <p className="text-1x1">PICK YOUR DESTINATION</p>{" "}
       </h2>
-      <div className="flex flex-row justify-between pl-48 pr-80">
-        <img src={current.images.webp} className="w-auto h-96 pt-24" alt="" />
-        <div className="flex flex-col ">
-          <div className="flex flex-row text-slate-400 pt-10 gap-4 pl-80 tracking-widest barlow-condensed-regular">
+      <div className="flex flex-col h-full lg:items-start items-center lg:flex-row lg:justify-between lg:pl-48 lg:pr-80">
+        <img src={current.images.webp} className="lg:w-auto pt-5 lg:h-96 h-[15rem]  lg:pt-24" alt="" />
+        <div className="flex items-center flex-col ">
+          <div className="flex flex-row pt-3 text-slate-400 lg:pt-10 gap-4 lg:pl-80 tracking-widest barlow-condensed-regular">
             <p
               className={`flex flex-row  cursor-pointer ${isActive === 0 ? 'border-b-2 border-white text-white' : ''}`}
               onClick={() => handleClick(0)}
@@ -54,15 +55,15 @@ const Destination = () => {
               TITAN
             </p>
           </div>
-          <div className="flex flex-col pl-[310px] pt-8">
-            <h1 className="text-white bellefair-regular text-8xl uppercase">
+          <div className="flex items-center flex-col lg:pl-[310px] lg:pt-8 pt-4">
+            <h1 className="text-white bellefair-regular lg:text-8xl text-6xl uppercase">
               {current.name}
             </h1>
-            <h2 className="text-slate-400 text-sm mt-5 barlow-regular leading-6">
+            <h2 className="text-slate-400 justify-center px-16 items-center text-center lg:text-sm text-sm mt-5 barlow-regular lg:leading-6">
               {current.description}
             </h2>
             <hr className="mt-5 border-slate-600" />
-            <div className="flex flex-row text-white mt-5 justify-between">
+            <div className="flex lg:flex-row flex-col gap-2 text-white mt-5 justify-between">
               <div className="flex flex-col">
                 <h1 className="barlow-condensed-regular leading-6 text-xs text-slate-400 tracking-widest">
                   AVG. DISTANCE
